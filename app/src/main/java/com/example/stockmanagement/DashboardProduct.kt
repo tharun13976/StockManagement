2acb9bbc91416366b48fe00bdefbd13a0008b71d
tharun <tharunmax13976@gmail.com>
2025-05-08 18:10:29 +0530

    Second step

--- app/src/main/java/com/example/stockmanagement/DashboardProduct.kt
+++ app/src/main/java/com/example/stockmanagement/DashboardProduct.kt
@@ -11,7 +11,7 @@ class DashboardProduct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard_product)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
